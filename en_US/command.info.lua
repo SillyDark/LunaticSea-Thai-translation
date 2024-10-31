c0b2a0dfc53e24f0ffaf3bfcc8ac1c886955b46d
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2024-10-31 18:42:01 +0700

    add: en_US translation for command.info

---
+++ en_US/command.info.lua
@@ -0,0 +1,6 @@
+return {
+  ping_desc = '**Ping:** `%s ms`',
+  invite_desc = '[Click here to invite!](%s)\n**Thanks for Inviting me in advance! 💫**',
+  dev_desc = '"Powered by Salmon :)\n - **Github:** %s\n - **Support server:** %s"',
+  dev_footer = 'Consider joining my server or inviting my bots :) This would help me a lot!'
+}
