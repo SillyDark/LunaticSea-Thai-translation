7fb8ad17da7fdc239758c2a1e9b0d865507e36f1
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2024-11-06 16:38:33 +0700

    add: full translation string for help command add: comments for easier to maintain

--- en_US/command.info.lua
+++ en_US/command.info.lua
@@ -1,8 +1,26 @@
 return {
+  -- Help command
+  ce_finder_name = '**Command:**',
+  ce_finder_des = '**Description:**',
+  ce_finder_usage = '**Usage:**',
+  ce_finder_access = '**Accessible by:**',
+  ce_finder_aliases = '**Aliases:**',
+  ce_finder_slash = '**Slash:**',
+  ce_finder_des_no = 'No Description',
+  ce_finder_usage_no = 'No Usage',
+  ce_finder_aliases_prefix = ' [Prefix only]',
+  ce_finder_aliases_no = 'No Aliases',
+  ce_finder_slash_enable = 'Enabled',
+  ce_finder_slash_disable = 'Disabled',
+  ce_finder_invalid = 'Invalid Command',
+  ce_finder_example = '`⚠️` | Please provide a valid **Command Name**!',
   ce_name = 'Help Command',
   ce_total = 'Total Commands: ',
+  -- Ping command
   ping_desc = '**Ping:** `%s ms`',
+  -- Invite command
   invite_desc = '**Thanks for Inviting me in advance! 💫**',
+  -- Developer command
   dev_desc = '"Powered by Salmon :)',
   dev_footer = 'Consider joining my server or inviting my bots :) This would help me a lot!'
 }
