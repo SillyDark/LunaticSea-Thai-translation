71e04115763874f58bce871f78bce44133e2842b
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2024-11-16 17:05:18 +0700

    add: utils translations for en_US

---
+++ en_US/command.utils.lua
@@ -0,0 +1,7 @@
+return {
+  -- Prefix command
+  prefix_set = '`㊙️` | Prefix has been set to **%s**',
+  prefix_change = '`㊙️` | Prefix has been changed to **%s**',
+  prefix_arg = '`⚠️` | Please provide a valid Prefix!',
+  prefix_length = '`⚠️` | Prefix can not be longer than **10 Characters**!',
+}
