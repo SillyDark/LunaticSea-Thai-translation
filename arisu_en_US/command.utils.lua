c5715c0cf9a5b546ba199f3ab2dd8e7b17e55d9f
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2024-11-18 12:39:09 +0700

    add: all translation string for utils commands

--- arisu_en_US/command.utils.lua
+++ arisu_en_US/command.utils.lua
@@ -10,4 +10,31 @@ return {
   lang_change = '`📙` | Language has been changed to **%s**!',
   lang_perm = '`⚠️` | You do not have permission to use this command!',
   provide_lang = "`⚠️` | Invalid Language. Please use:\n`%s`",
+
+  -- SongNoti command!
+  songnoti_set = '`🔔` | Song Notifications has been set to **%s**!',
+  songnoti_already = '`⚠️` | Song Notifications are already **%s**!',
+
+  -- 24/7 command!
+  two47_on = '`✅` | 24/7 mode is now **Enabled**!',
+  two47_off = '`❌` | 24/7 mode is now **Disabled**!',
+  two47_already = '`⚠️` | 24/7 mode is already **%s**!',
+
+  -- Setup command!
+  setup_msg = "**`⚙️` | Created Music System** %s\n```Note: Do not delete any messages sent by me for the Music System to work properly!```",
+  setup_deleted = '**`⚙️` | Deleted Music System!**',
+  setup_topic = "⏹ **Stop**\n⏮️ **Previous**\n⏯ **Pause/Resume**\n⏭️ **Skip**\n🔁 **Loop/Unloop**",
+  setup_enable = '`⚠️` | Music System already exists!',
+  setup_null = '`⚠️` | Music System not found!',
+
+  -- Blacklist command!
+  bl_invalid_mode = '`❌` | Invalid type of mode, please use add / remove only',
+  bl_invalid_type = '`❌` | Invalid type, please use guild / user only',
+  bl_add = '`✅` | Blacklisted id `%s` successfully!',
+  bl_remove = '`✅` | Blacklisted id `%s` removed successfully!',
+
+  -- Max length
+  ml_invalid = '`⚠️` | Invalid time. Please use a valid timestamp. Example: **1:30**!',
+  ml_set = '`✅` | Successfully set the max length of song to `%s`',
+  ml_remove = '`✅` | Successfully remove the max length of the song',
 }
