e4a1872a8903f73703c03b23526d59c4eb78b939
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2025-01-07 19:22:25 +0700

    fix: unstable player destroy checker

--- arisu_en_US/command.music.lua
+++ arisu_en_US/command.music.lua
@@ -3,4 +3,7 @@ return {
   join_msg = '`🔊` | Joined %s',
   join_voice = '`⚠️` | You need to be in a voice channel to use this command!',
   join_already = '`⚠️` | I am already in %s',
+
+  -- Stop command!
+  stop_msg = '`⏹️` | Stopped the **Player**!',
 }
