c4f18ead6e25d8811a5acf2b46ea97b19711afe1
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2025-01-10 17:11:26 +0700

    add: full port of button.music

---
+++ arisu_en_US/button.music.lua
@@ -0,0 +1,40 @@
+return {
+  queue_author = '💾 %s',
+  queue_description = "`📀` | Currently Playing:\n**%s** `[%s]` • %s\n\n`💤` | Rest of Queue:**%s**",
+  queue_footer = 'Page • %s/%s | Songs • %s | Total Duration • %s',
+  -- Pause
+  pause_msg = '`⏸️` | Paused the **Player**!',
+  resume_msg = '`▶️` | Resumed the **Player**!',
+  -- Skip
+  skip_msg = '`⏭️` | Skipped the **Song**!',
+  skip_notfound = '`⚠️` | No song was found to Skip to!',
+  -- Stop
+  stop_msg = '`⏹️` | Stopped the **Player**!',
+  -- Volume
+  volup_msg = '`🔊` | Changed Volume to **%s%**',
+  voldown_msg = '`🔉` | Changed Volume to **%s%**',
+  volume_invalid = '`⚠️` | Invalid Volume. Please use a number between **1** and **100**!',
+  volume_max = '`⚠️` | Player is already at max volume!',
+  volume_min = '`⚠️` | Player is already at min volume!',
+  -- Clear
+  clear_msg = '`🗑️` | Queue has been **Cleared**!',
+  -- Loop
+  loop_current = '`🔁` | Looping current **Song**!',
+  loop_all = '`🔁` | Looping **Queue**!',
+  unloop_all = '`❌` | Loop is now **Disabled**!',
+  loop_invalid = '`⚠️` | Invalid Loop mode. Please use %s!',
+  loop_already = '`⚠️` | Loop mode is already **%s**!',
+  -- Previous
+  previous_msg = '`⏮️` | Skipped to Previous **Song**!',
+  previous_notfound = '`⚠️` | No Previous song was found!',
+  -- Shuffle
+  shuffle_msg = '🔀 Shuffled the Queue',
+
+  -- All filter command!
+  filter_on = '`✅` | %s filter is now **enable**!',
+  filter_already = '`⚠️` | %s filter is already **enabled**!',
+
+  -- Reset command!
+  reset_on = '`❌` | Filter has been **Reset**!',
+  reset_already = '`⚠️` | Filter has already been **Reset**!',
+}
